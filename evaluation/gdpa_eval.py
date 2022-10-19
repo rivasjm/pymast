4e95522237fb7102511eb26108fe3b7309b0a577
Juan M Rivas <rivasjm@unican.es>
2022-10-19 19:36:24 +0200

    update

--- evaluation/gdpa_eval.py
+++ evaluation/gdpa_eval.py
@@ -1,44 +1,55 @@
 from analysis import reset_wcrt, HolisticAnalyis
 from assignment import PDAssignment, HOPAssignment, RandomAssignment
 from gradient_descent import *
+from generator import set_utilization
 import itertools
 import numpy as np
 from examples import get_medium_system
 from random import Random
 from multiprocessing import Pool
 from datetime import datetime
+import pandas as pd
 
 
-lrs = [0.1, 0.5, 1, 2]
-deltas = [0.5, 1, 1.5]
-beta1s = [0.8, 0.9]
-beta2s = [0.9, 0.999]
-epsilons = [0.1, 0.01]
+lrs = [2]
+deltas = [1.5]
+beta1s = [0.9]
+beta2s = [0.999]
+epsilons = [0.1]
 population = 50
-utilization = 0.6
+utilization_min = 0.5
+utilization_max = 0.9
+utilization_steps = 20
 
 
 def parameters_comparison():
     random = Random(42)
-    systems = [get_medium_system(random, utilization) for _ in range(population)]
+    utilizations = np.linspace(utilization_min, utilization_max, utilization_steps)
+    systems = [get_medium_system(random, utilization_min) for _ in range(population)]
     names, _ = zip(*get_assignments(lrs, deltas, beta1s, beta2s, epsilons))
-    results = np.zeros(len(names))
+    results = np.zeros((len(names), len(utilizations)))
 
     i = 0
-    with Pool(4) as pool:
-        for arr in pool.imap_unordered(step, systems):
-            i += 1
-            print(f"population={i}")
-            results += arr
-            if i % 5 == 0:
-                overview(i, names, results)
+    for u, utilization in enumerate(utilizations):
+        # set utilization to the generated systems
+        for system in systems:
+            set_utilization(system, utilization)
+
+        #
+        with Pool(1) as pool:
+            for arr in pool.imap_unordered(step, systems):
+                i += 1
+                print(f"population={i}")
+                results[:, u] += arr
+                if i % 5 == 0:
+                    overview(i, names, results)
 
     overview("final", names, results, save=True)
 
 
 def overview(i, names, results, save=False):
     if save:
-        np.savetxt(f"gdpa_eval_2_{i}.csv", results, delimiter=",", header=" | ".join(names))
+        np.savetxt(f"gdpa{i}.csv", results, delimiter=",", header=",".join(utilizations))
     print(f"{datetime.now()} : population={i}")
     for name, res in zip(names, results):
         print(f"  {name} = {res}")
@@ -49,6 +60,7 @@ def step(system):
     sched_test = get_sched_test()
     results = np.zeros(len(assigs))
     for a, assig in enumerate(assigs):
+        print(names[a])
         if achieves_schedulability(system, assig, sched_test):
             results[a] += 1
     return results
