095f1e72c9fb945ab99494af6d275384831bf991
Juan M Rivas <rivasjm@unican.es>
2022-10-20 19:40:19 +0200

    update

--- evaluation/gdpa_eval.py
+++ evaluation/gdpa_eval.py
@@ -9,9 +9,11 @@ from random import Random
 from multiprocessing import Pool
 from datetime import datetime
 import pandas as pd
+from functools import partial
+import matplotlib.pyplot as plt
 
 
-lrs = [2]
+lrs = [2, 3]
 deltas = [1.5]
 beta1s = [0.9]
 beta2s = [0.999]
@@ -29,41 +31,66 @@ def parameters_comparison():
     names, _ = zip(*get_assignments(lrs, deltas, beta1s, beta2s, epsilons))
     results = np.zeros((len(names), len(utilizations)))
 
-    i = 0
+    job = 0
     for u, utilization in enumerate(utilizations):
         # set utilization to the generated systems
         for system in systems:
             set_utilization(system, utilization)
 
         #
-        with Pool(1) as pool:
-            for arr in pool.imap_unordered(step, systems):
-                i += 1
-                print(f"population={i}")
-                results[:, u] += arr
-                if i % 5 == 0:
-                    overview(i, names, results)
+        with Pool(4) as pool:
+            func = partial(step, index=u)
+            for arr, index in pool.imap_unordered(func, systems):
+                job += 1
+                results[:, index] += arr
+                if job % 25 == 0:
+                    print(f"{datetime.now()} : job={job}")
+                if job % population == 0:
+                    excel("1", names, utilizations, results)
+                    chart("1", names, utilizations, results, save=True)
 
-    overview("final", names, results, save=True)
+    excel("1", names, utilizations, results)
+    chart("1", names, utilizations, results, save=True)
 
 
-def overview(i, names, results, save=False):
+def print_overview(label, names, utilizations, results):
+    df = pd.DataFrame(data=results,
+                      index=names,
+                      columns=utilizations)
+    print(df)
+
+
+def chart(label, names, utilizations, results, save=False):
+    # the export version should be transposed, it is the convention to have the continuous data in the columns
+    df = pd.DataFrame(data=np.transpose(results),
+                      index=utilizations,
+                      columns=names)
+    fig, ax = plt.subplots()
+    df.plot(ax=ax)
+    ax.set_ylabel("Schedulable systems")
+    ax.set_xlabel("Average utilization")
     if save:
-        np.savetxt(f"gdpa{i}.csv", results, delimiter=",", header=",".join(utilizations))
-    print(f"{datetime.now()} : population={i}")
-    for name, res in zip(names, results):
-        print(f"  {name} = {res}")
+        fig.savefig(f"gdpa_{label}.png")
+    plt.show()
 
 
-def step(system):
+def excel(label, names, utilizations, results):
+    # the export version should be transposed, it is the convention to have the continuous data in the columns
+    df = pd.DataFrame(data=np.transpose(results),
+                      index=utilizations,
+                      columns=names)
+    df.to_excel(f"gdpa_{label}.xlsx")
+
+
+def step(system, index):
     names, assigs = zip(*get_assignments(lrs, deltas, beta1s, beta2s, epsilons))
     sched_test = get_sched_test()
     results = np.zeros(len(assigs))
     for a, assig in enumerate(assigs):
-        print(names[a])
+        # print(names[a])
         if achieves_schedulability(system, assig, sched_test):
             results[a] += 1
-    return results
+    return results, index
 
 
 def achieves_schedulability(system, assignment, analysis) -> bool:
@@ -76,13 +103,27 @@ def achieves_schedulability(system, assignment, analysis) -> bool:
 def get_assignments(lrs, deltas, beta1s, beta2s, epsilons):
     analysis = HolisticAnalyis(reset=False, limit_factor=5)
     params = itertools.product(lrs, deltas, beta1s, beta2s, epsilons)
-    assigs = [("pd", PDAssignment(normalize=True))]
+
+    pd = PDAssignment(normalize=True)
+    hopa = HOPAssignment(normalize=True, analysis=analysis)
+
+    assigs = [("pd", pd), ("hopa", hopa)]
 
     for lr, delta, beta1, beta2, epsilon in params:
-        assig = GDPA(proxy=analysis, verbose=False, initial=RandomAssignment(normalize=True, random=Random(123)),
+        assig = GDPA(proxy=analysis, verbose=False, initial=RandomAssignment(normalize=True),
                      iterations=200, cost_fn=invslack, analysis=analysis, delta=delta,
                      optimizer=Adam(lr=lr, beta1=beta1, beta2=beta2, epsilon=epsilon))
-        assigs.append((f"lr={lr} d={delta} b1={beta1} b2={beta2} e={epsilon}", assig))
+        assigs.append((f"gdpa-r [lr={lr} d={delta} b1={beta1} b2={beta2} e={epsilon}]", assig))
+
+        assig = GDPA(proxy=analysis, verbose=False, initial=pd,
+                     iterations=200, cost_fn=invslack, analysis=analysis, delta=delta,
+                     optimizer=Adam(lr=lr, beta1=beta1, beta2=beta2, epsilon=epsilon))
+        assigs.append((f"gdpa-pd [lr={lr} d={delta} b1={beta1} b2={beta2} e={epsilon}]", assig))
+
+        assig = GDPA(proxy=analysis, verbose=False, initial=hopa,
+                     iterations=200, cost_fn=invslack, analysis=analysis, delta=delta,
+                     optimizer=Adam(lr=lr, beta1=beta1, beta2=beta2, epsilon=epsilon))
+        assigs.append((f"gdpa-hopa [lr={lr} d={delta} b1={beta1} b2={beta2} e={epsilon}]", assig))
 
     return assigs
 
