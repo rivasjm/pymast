84b95bd56398449c88edc02a9df8b1cb44f5eac6
juan <jmrivasconcepcion@gmail.com>
2022-11-19 13:19:48 +0100

    update

--- evaluation/brute_force_anomaly.py
+++ evaluation/brute_force_anomaly.py
@@ -3,9 +3,15 @@
 def analyze_log(file):
     with open(file) as f:
         for line in f.readlines():
-            tools = line.split("->")[1].strip()
-            if "bf" not in tools and tools:
+            if has_anomaly(line):
                 print(line.rstrip("\n"))
+
+
+def has_anomaly(line):
+    tools = line.split("->")[1].strip()
+    if "bf" not in tools and tools:
+        return True
+    return False
 
 
 if __name__ == '__main__':
